3eb44528c4a050ea44fc11df28fe314e14573342
Tyler Diderich <tyler@Tylers-MacBook-Pro.local>
2021-12-01 15:20:11 -0600

    cleanup

--- gmail_api.py
+++ gmail_api.py
@@ -9,7 +9,6 @@ import requests
 import re
 import os
 
-# If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly',
           'https://www.googleapis.com/auth/gmail.modify']
 PRIMER_API_KEY = os.getenv('PRIMER_API_KEY')
